4d9b7e9c722edebe8f725d1595f5128dd7152862
muhammadgufindoalenra <mgufindo@gmail.com>
2022-03-20 22:49:32 +0700

    session logout

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Helper\CurlHelper;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class AdminController extends Controller
 {
